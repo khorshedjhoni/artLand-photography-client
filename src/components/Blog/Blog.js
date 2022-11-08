5d29922d626099d910f79fd93c32381a90f6bd96
unknown <khorshedjhoni@gmail.com>
2022-11-08 21:45:00 +0600

    blog ques-1 added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>this is blog</h2>
+            <div>
+                <h3> what is the difference between SQL and NoSQL?</h3>
+                <p>Most SQL databases can be scaled vertically, by increasing the processing power of existing hardware. NoSQL databases use a master-slave architecture which scales better horizontally, with additional servers or nodes. SQL database schemata always represent relational, tabular data, with rules about consistency and integrity. They contain tables with columns (attributes) and rows (records), and keys have constrained logical relationships.NoSQL databases need not stick to this format, but generally fit into one of four broad categories:QL databases represent massive communities, stable code bases, and proven standards. Multitudes of examples are posted online and experts are available to support those new to programming relational data.NoSQL technologies are being adopted quickly, but communities remain smaller and more fractured. However, many SQL languages are proprietary or associated with large single-vendors, while NoSQL communities benefit from open systems and concerted commitment to onboarding users.</p>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
