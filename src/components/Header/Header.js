19f44d565dfd852540f21c4cd2d418038c67fd74
unknown <khorshedjhoni@gmail.com>
2022-11-10 21:56:32 +0600

    navbar responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,12 +13,14 @@ const Header = () => {
         .catch(error=>console.error(error))
       }
     return (
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">ARTLAND PHOTOGRAPHY</Navbar.Brand>
+       <div>
+       < Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">ART LAND PHOTOGRAPHY</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-           
-            <NavLink className='link' to='/'>Home</NavLink>
+          <NavLink className='link' to='/'>Home</NavLink>
             <NavLink className='link' to='/blog'>Blog</NavLink> 
                         
             <NavLink className='link' to='/login'>
@@ -42,11 +44,48 @@ const Header = () => {
                 </NavLink>     
                        
             <NavLink className='link' to='/services'>Services</NavLink>
-            
+
+
+            {/* <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+           
           </Nav>
-        </Container>
-      </Navbar>
+          {/* <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+       </div>
     );
 };
 
 export default Header;
+
+{/* <NavLink className='link' to='/'>Home</NavLink>
+            <NavLink className='link' to='/blog'>Blog</NavLink> 
+                        
+            <NavLink className='link' to='/login'>
+                   
+                {
+                    user?.uid?
+                    
+                    <>
+                    
+              
+                    <button className='bg-dark text-white' onClick={handleLogOut}>logOut</button>
+                    <NavLink className='link' to='/addServices'>Add Services</NavLink> 
+              
+                    </>
+                    :
+                    <>
+                     <NavLink className='text-white text-decoration-none' to ='/login'>login</NavLink>
+                     
+                    </> 
+                }  
+                </NavLink>     
+                       
+            <NavLink className='link' to='/services'>Services</NavLink> */}
